79b0b3931342ced1a57d2495ba17526286745dcc
Fred <tredy-94@outlook.com>
2026-05-14 00:14:18 -0500

    feat: deploy client-app to Vercel

--- client-app/app/page.js
+++ client-app/app/page.js
@@ -1,6 +1,12 @@
-import { redirect } from "next/navigation"
+export default function Home() {
+  return <h1>BarLink funciona</h1>
+}
+
+/* import { redirect } from "next/navigation"
 export default function Home() {
   redirect("/mesa/1")
+} */
+
 /* export default function Home() {
   return (
     <div style={{
@@ -22,4 +28,3 @@ export default function Home() {
       </p>
     </div>
   ) */
-}
