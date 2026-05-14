96f6a12f725668d313a7965381e6e47ca8ce72fe
Fred <tredy-94@outlook.com>
2026-05-13 19:27:19 -0500

    fix: add valid home page component

---
+++ client-app/app/page.js
@@ -0,0 +1,24 @@
+import { redirect } from "next/navigation"
+
+export default function Home() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      background: "#0a0a0f",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      color: "white",
+      fontFamily: "sans-serif"
+    }}>
+      <h1 style={{ fontSize: 32, fontWeight: 700 }}>
+        <span style={{ color: "#d63af9" }}>Bar</span>
+        <span style={{ color: "#00d4ff" }}>Link</span>
+      </h1>
+      <p style={{ color: "#8888aa", marginTop: 8 }}>
+        Escanea el QR de tu mesa para comenzar
+      </p>
+    </div>
+  )
+}
